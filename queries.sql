baa76c5ff82a673b7de59b4423c8952846dc3712
Liliia Ivanovskaia <109848147+livanovskaya@users.noreply.github.com>
2025-09-17 17:46:02 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -49,4 +49,47 @@ from (select concat(first_name, ' ', last_name)                                 
 order by numeric_day, seller;
 
 
+--- анализ по возрастным группам
+select case
+           when age >= 16 and age <= 25 then '16-25'
+           when age >= 26 and age <= 40 then '26-40'
+           else '40+' end             age_category,
+       count(distinct customer_id) as age_count
+from customers
+group by 1
+order by age_category;
+
+
+---- кол-во покупателей и выручка по месяцам
+select TO_CHAR(sale_date, 'YYYY-MM') as selling_month,
+       count(distinct s.customer_id) as total_customers,
+       floor(sum(price * quantity))        as income
+from sales s
+         left join customers c on c.customer_id = s.customer_id
+         left join products p on p.product_id = s.product_id
+group by 1
+order by selling_month;
+
+--- выгрузка по первой акционной покупке
+with sales_agg as (select sales_id, min_order_date, sale_date
+                   from (select c.customer_id,
+                                sales_id,
+                                min(sale_date) over (partition by c.customer_id) as min_order_date,
+                                sale_date
+                         from sales s
+                                  left join customers c on c.customer_id = s.customer_id) as a
+                   where sale_date = min_order_date)
+
+select (c.first_name || ' ' || c.last_name) as customer,
+                sale_date,
+                (e.first_name || ' ' || e.last_name) as seller
+from sales s
+         left join customers c on c.customer_id = s.customer_id
+         left join products p on p.product_id = s.product_id
+         left join employees e on e.employee_id = s.sales_person_id
+where price = 0
+  and sales_id in (select sales_id from sales_agg)
+group by c.customer_id, customer, sale_date, seller
+order by c.customer_id;
+
 
